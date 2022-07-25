7536306c760ddad0e75ba0e942fdf1fea7b36314
clairelarson <58090540+clairelarson@users.noreply.github.com>
2022-07-25 15:08:29 +0900

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -11,20 +11,23 @@ def call_statistics(list):
     median = statistics.median(list)
     mode = statistics.mode(list)
     print(f"Your total number of attempts was {tot_att}")
-    print(f"Your mean number of attempts per game was {mean}")
-    print(f"Your median number of attemtps per game was {median}")
-    print(f"Your mode number of attemtps per game was {mode}")
-
+    print(f"Your mean, median and mode number of attempts per game were: {mean}, {median}, {mode}")
+    
 def start_game(game_attempts):
     attempts = 0
     # A random number should be chosen that is within the range of 1 - 100.
     random_num = random.randint(1, 100)
     print(random_num)
-    run = True
     # As a player of the game, I should see some kind of text header, welcome, or game intro message
-    print("Hello! Welcome to the random number game!")
+    # show the current best score
+    if game_attempts != []:
+        best_score = min(game_attempts)
+        print(f"Hello! Welcome to the random number game! The current best score is {best_score}")
+    else:
+        best_score = 0
+        print(f"Hello! Welcome to the random number game! The current best score is {best_score}")
     try:
-        while run:
+        while True:
             guess = int(input("Please guess a whole number between 1 and 100:  "))
             # continuously prompted for a guess until I get it right.
             # I should be told if my answer is higher or lower 
@@ -46,9 +49,7 @@ def start_game(game_attempts):
                 # save the player’s number of attempts at guessing to a list.
                 game_attempts.append(attempts)
                 print(f"You got it! Number of attempts: {attempts}")
-                # show the current best score
-                best_score = min(game_attempts)
-                print(f"The current best score is {best_score}")
+                #best_score = min(game_attempts)
                 # I should be prompted if I would like to play again.
                 cont_game = input("Would you like to play again? Yes or No  ")
                 if cont_game.upper() == "YES":
@@ -57,6 +58,7 @@ def start_game(game_attempts):
                 elif cont_game.upper() == "NO":
                     # ending message is shown to the player.
                     print("~Okay, thanks for playing~")
+                    print(f"Your best score was {best_score}!")
                     call_statistics(game_attempts)
                     exit()
                 #catch possible errors in input
@@ -66,5 +68,6 @@ def start_game(game_attempts):
     #catch possible errors in input
     except ValueError:
         print("Invalid response, please type a whole number from 1-100")
+        start_game(game_attempts)
 
 start_game(total_attempts)
